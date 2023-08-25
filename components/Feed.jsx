2f7862547e901b6071146ca646da5429df1d1565
AryanK1511 <aryankhurana1511@gmail.com>
2023-08-25 16:00:57 -0400

    Fixed the formatting for components

--- components/Feed.jsx
+++ components/Feed.jsx
@@ -1,61 +1,54 @@
-"use client"
+"use client";
 
-import React, { useState, useEffect } from 'react';
-import PromptCard from './PromptCard';
+import React, { useState, useEffect } from "react";
+import PromptCard from "./PromptCard";
 
 const PromptCardList = ({ data, handleTagClick }) => {
   return (
-    <div className='mt-16 prompt_layout'>
-      {
-        data.map((post) => (
-          <PromptCard 
-            key={post._id}
-            post={post}
-            handleTagClick={handleTagClick}
-          />
-        ))
-      }
+    <div className="mt-16 prompt_layout">
+      {data.map((post) => (
+        <PromptCard
+          key={post._id}
+          post={post}
+          handleTagClick={handleTagClick}
+        />
+      ))}
     </div>
-  )
-}
+  );
+};
 
 const Feed = () => {
   const [searchText, setsearchText] = useState("");
-  const [ posts, setPosts ] = useState([]);
+  const [posts, setPosts] = useState([]);
 
-  const handleSearchChange = (e) => {
-    
-  }
+  const handleSearchChange = (e) => {};
 
   useEffect(() => {
     const fetchPosts = async () => {
       const response = await fetch("/api/prompt");
       const data = await response.json();
       setPosts(data);
-    }
+    };
 
     fetchPosts();
-  }, [])
+  }, []);
 
   return (
-    <section className='feed'>
-      <form className='relative w-full flex-center'>
-        <input 
-          type='text'
-          placeholder='Search for a tag or a username'
+    <section className="feed">
+      <form className="relative w-full flex-center">
+        <input
+          type="text"
+          placeholder="Search for a tag or a username"
           value={searchText}
           onChange={handleSearchChange}
           required
-          className='search_input peer'
+          className="search_input peer"
         />
       </form>
 
-      <PromptCardList
-        data={posts}
-        handleTagClick={() => {}}
-      />
+      <PromptCardList data={posts} handleTagClick={() => {}} />
     </section>
-  )
-}
+  );
+};
 
-export default Feed
+export default Feed;
